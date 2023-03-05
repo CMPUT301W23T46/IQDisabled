f3c19c93ee854028ecf7ee816813ecc5a82c409d
ZacharyZ <yiyang21@ualberta.ca>
2023-03-05 00:02:43 -0700

    updated initial file contain log in page

--- QRCodeHunter/app/src/main/java/com/example/qr/MainActivity.java
+++ QRCodeHunter/app/src/main/java/com/example/qr/MainActivity.java
@@ -24,5 +24,11 @@ public class MainActivity extends AppCompatActivity {
                 
             }
         });
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
