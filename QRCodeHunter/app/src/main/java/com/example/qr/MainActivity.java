bd5757b6b1228b7c13e8fc3e07b982ef04c64e6c
ZacharyZ <yiyang21@ualberta.ca>
2023-03-04 22:00:03 -0700

    updated initial file contain log in page

--- QRCodeHunter/app/src/main/java/com/example/qr/MainActivity.java
+++ QRCodeHunter/app/src/main/java/com/example/qr/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.qr;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +16,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button signInButton = (Button) findViewById(R.id.sign_in_button);
-        
+        TextView registerButton = (TextView)findViewById(R.id.register_button);
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
     }
 }
