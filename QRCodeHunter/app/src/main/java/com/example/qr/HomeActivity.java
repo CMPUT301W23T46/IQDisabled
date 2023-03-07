f583a87bebf0737a1f5e55dd9f91e7cf5f563892
Wang <u7095437@anu.edu.au>
2023-03-07 11:37:05 -0700

    Added each activity along with xml

--- QRCodeHunter/app/src/main/java/com/example/qr/HomeActivity.java
+++ QRCodeHunter/app/src/main/java/com/example/qr/HomeActivity.java
@@ -1,7 +1,9 @@
 package com.example.qr;
 
 import android.content.SharedPreferences;
+import android.media.Image;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,9 +17,15 @@ public class HomeActivity extends AppCompatActivity {
         String username = sharedPref.getString("username","N/A");
         String email = sharedPref.getString("email","N/A");
         String phone = sharedPref.getString("phone","N/A");
-        System.out.println(username);
-        System.out.println(email);
-        System.out.println(phone);
+//        System.out.println(username);
+//        System.out.println(email);
+//        System.out.println(phone);
+        ImageButton contactBtn = findViewById(R.id.contact_btn);
+        ImageButton addBtn = findViewById(R.id.add_btn);
+        ImageButton mapBtn = findViewById(R.id.map_btn);
+        ImageButton profileBtn = findViewById(R.id.profile_btn);
+
+
 
     }
 }
