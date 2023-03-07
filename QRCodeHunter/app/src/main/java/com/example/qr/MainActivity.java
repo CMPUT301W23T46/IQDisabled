3f2cb7e7f1d43fd440de5d89b2648b9d815f0c6b
“Yile <yile@ualberta.ca>
2023-03-07 13:01:23 -0700

    3.7

--- QRCodeHunter/app/src/main/java/com/example/qr/MainActivity.java
+++ QRCodeHunter/app/src/main/java/com/example/qr/MainActivity.java
@@ -10,7 +10,7 @@ import android.widget.TextView;
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button remove_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,10 +18,13 @@ public class MainActivity extends AppCompatActivity {
         Button signInButton = (Button) findViewById(R.id.sign_in_button);
         TextView registerButton = (TextView)findViewById(R.id.register_button);
 
+
+
+
         signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                setContentView(R.layout.sign_in_page);
+                setContentView(R.layout.activity_sign_in);
                 Button cancel = (Button) findViewById(R.id.cancel_button);
                 
             }
@@ -29,11 +32,25 @@ public class MainActivity extends AppCompatActivity {
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                setContentView(R.layout.register_page);
+                setContentView(R.layout.activity_sign_up);
                 Button cancel = (Button) findViewById(R.id.cancel_button);
 
 
             }
         });
+
+
+
+
+
+
+
+
+
     }
+
+
+
+
+
 }
