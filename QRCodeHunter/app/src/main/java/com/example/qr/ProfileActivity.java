4aa674f7a7740d8f2689ef941cf4ee60c9abd318
Wang <u7095437@anu.edu.au>
2023-03-08 04:25:57 -0700

    Profile Page Updated

--- QRCodeHunter/app/src/main/java/com/example/qr/ProfileActivity.java
+++ QRCodeHunter/app/src/main/java/com/example/qr/ProfileActivity.java
@@ -5,8 +5,11 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import org.w3c.dom.Text;
 
 public class ProfileActivity extends AppCompatActivity {
     @Override
@@ -24,6 +27,14 @@ public class ProfileActivity extends AppCompatActivity {
         ImageButton addBtn = findViewById(R.id.add_btn);
         ImageButton mapBtn = findViewById(R.id.map_btn);
         ImageButton contactBtn = findViewById(R.id.contact_btn);
+
+        TextView u_profile_name = findViewById(R.id.edit_user_name);
+        TextView u_profile_phone = findViewById(R.id.edit_phone_number);
+        TextView u_profile_email = findViewById(R.id.edit_email_address);
+
+        u_profile_email.setText(email);
+        u_profile_name.setText(username);
+        u_profile_phone.setText(phone);
 
         homeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
