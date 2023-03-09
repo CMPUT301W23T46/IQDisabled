079e489b2a5f02d170908dedbf1d763ae069c484
FuxianZhang <fuxian@ualberta.ca>
2023-03-09 13:24:52 -0700

    Map activity achieved

--- QRCodeHunter/app/src/main/java/com/example/qr/MapActivity.java
+++ QRCodeHunter/app/src/main/java/com/example/qr/MapActivity.java
@@ -8,11 +8,37 @@ import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MapActivity extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import android.Manifest;
+
+import android.content.pm.PackageManager;
+
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener{
+    private static final int REQUEST_LOCATION_PERMISSION = 1;
+    private GoogleMap mMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.maps);
+        mapFragment.getMapAsync(this);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.ACCESS_FINE_LOCATION }, REQUEST_LOCATION_PERMISSION);
+        }
         SharedPreferences sharedPref = getSharedPreferences("myPref", MODE_PRIVATE);
         String username = sharedPref.getString("username","N/A");
         String email = sharedPref.getString("email","N/A");
@@ -56,5 +82,55 @@ public class MapActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_LOCATION_PERMISSION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission granted
+            } else {
+                // Permission denied
+            }
+        }
+    }
+
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setOnMapClickListener(this);
+        double latitude = 53.524649;
+        double longitude = -113.526830;
+        MarkerOptions markerOptions = new MarkerOptions()
+                .position(new LatLng(latitude, longitude))
+                .title("University of Alberta CAB")
+                .snippet("Central Academic Building");
+        Marker marker = mMap.addMarker(markerOptions);
+        marker.setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
+        marker.setAlpha(0.8f);
+        marker.setZIndex(1.0f);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(latitude, longitude), 16));
+
+        // Enable the my location button and request permission to access the user's location if necessary
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            googleMap.setMyLocationEnabled(true);
+        } else {
+            ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.ACCESS_FINE_LOCATION }, REQUEST_LOCATION_PERMISSION);
+        }
+
+        // Add a listener for when the user clicks on the map
+
+    }
+
+    @Override
+    public void onMapClick(LatLng latLng){
+        double latitude = latLng.latitude;
+        double longitude = latLng.longitude;
+        Log.d("MapActivity","Latitude: " + latitude + "Longitude: " + longitude);
+
+        String message = "Latitude: " + latitude +"Longitude " + longitude;
+        Toast.makeText(this,message,Toast.LENGTH_SHORT).show();
     }
 }
