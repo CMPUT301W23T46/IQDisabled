4105f0ccaf50fb186750760faeafd57897fbca1f
“Yile <yile@ualberta.ca>
2023-03-09 19:10:56 -0700

    edited back button in statistic page

--- QRCodeHunter/app/src/main/java/com/example/qr/StatisticsActivity.java
+++ QRCodeHunter/app/src/main/java/com/example/qr/StatisticsActivity.java
@@ -2,7 +2,10 @@ package com.example.qr;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class StatisticsActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class StatisticsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistics);
-    }
+        ImageButton backButton = findViewById(R.id.imageButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StatisticsActivity.this, MyQRCodeActivity.class);
+                startActivity(intent);
+            }
+        });    }
 }
