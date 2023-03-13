f4e7be1dbf297b9f8793adcca1df3752930a54ca
FuxianZhang <fuxian@ualberta.ca>
2023-03-13 13:20:46 -0600

    homeactivity updated

---
+++ QRCodeHunter/app/src/test/java/com/example/qr/QRCodeStatsTest.java
@@ -0,0 +1,34 @@
+package com.example.qr;
+
+import org.junit.Test;
+
+import java.security.NoSuchAlgorithmException;
+
+public class QRCodeStatsTest {
+    @Test
+    public static String naming(String firstSixBits) throws Exception {
+
+    }
+
+    @Test
+    public int scoring(String hexString) throws Exception {
+
+    }
+
+    @Test
+    public static String hashString() throws NoSuchAlgorithmException {
+        String input =
+    }
+
+    @Test
+    public static String extractFirstSix(String hash) throws Exception {
+
+    }
+
+    @Test
+    public String visualRep(String firstSixBits) throws Exception {
+
+    }
+
+
+}
