5c7f208f62108be0aaac24936e5fbd3ac8a5f898
Wang <u7095437@anu.edu.au>
2023-04-03 10:11:30 -0600

    Fixed bugs on HomeActivity.java and removed test usage data

--- QRCodeHunter/app/src/main/java/com/example/qr/HomeActivity.java
+++ QRCodeHunter/app/src/main/java/com/example/qr/HomeActivity.java
@@ -50,25 +50,26 @@ public class HomeActivity extends AppCompatActivity {
 
 
 
-        DataBaseHelper dbhelper = new DataBaseHelper();
-//        Integer score = dbhelper.getQRcodeScore("2f0eb1859e295bcd183127558f3c205270e7a8004ad362e5123bd5b2774e0f9c");
-
-        dbhelper.getQRcodeScore("2f0eb1859e295bcd183127558f3c205270e7a8004ad362e5123bd5b2774e0f9c", new OnGetQRCodeScoreListener() {
-            @Override
-            public void onSuccess(Integer score) {
-                Toast.makeText(HomeActivity.this, score.toString(), Toast.LENGTH_SHORT).show();
-            }
-        });
+//        DataBaseHelper dbhelper = new DataBaseHelper();
+////      Integer score = dbhelper.getQRcodeScore("2f0eb1859e295bcd183127558f3c205270e7a8004ad362e5123bd5b2774e0f9c");
+//
+//        dbhelper.getQRcodeScore("2f0eb1859e295bcd183127558f3c205270e7a8004ad362e5123bd5b2774e0f9c", new OnGetQRCodeScoreListener() {
+//            @Override
+//            public void onSuccess(Integer score) {
+//                Toast.makeText(HomeActivity.this, score.toString(), Toast.LENGTH_SHORT).show();
+//            }
+//        });
 
 
 //        Toast.makeText(this, player1.getEmail()+player1.getPhone_number(), Toast.LENGTH_SHORT).show();
-                contactBtn.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent intent = new Intent(HomeActivity.this, ContactActivity.class);
-                        startActivity(intent);
-                    }
-                });
+
+        contactBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, ContactActivity.class);
+                startActivity(intent);
+            }
+        });
         addBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
